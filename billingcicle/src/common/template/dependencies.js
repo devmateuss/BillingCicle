09fc640434ed28956611d985220fe2123d1a01e0
Mateus <mateus.sousa@deway.com.br>
2019-07-18 16:32:17 -0300

    config styles adminlte

---
+++ billingcicle/src/common/template/dependencies.js
@@ -0,0 +1,13 @@
+import 'admin-lte/plugins/jQueryUI/jquery-ui.min'
+import 'admin-lte/bower_components/fastclick/lib/fastclick'
+import 'admin-lte/bower_components/jquery-slimscroll/jquery.slimscroll'
+import 'admin-lte/dist/js/adminlte.min'
+
+import 'font-awesome/css/font-awesome.min.css'
+import 'modules/ionicons/dist/css/ionicons.min.css'
+import 'admin-lte/bower_components/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/admin-lte/dist/css/AdminLTE.min.css'
+import 'modules/admin-lte/dist/css/skins/_all-skins.min.css'
+import 'modules/admin-lte/plugins/iCheck/flat/blue.css'
+
+import './custom.css'
